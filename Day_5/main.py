87d007c6ada438dead821bf13e5633faea5fc733
kabir5145 <kabir10112006@gmail.com>
2025-10-18 00:18:02 +0530

    Comments,Escape Sequences & Print Statements in python course, Day 5

---
+++ Day_5/main.py
@@ -0,0 +1,11 @@
+# Hey Harry , Please don't remove this line
+# Author : Harry
+# Course : 100DaysOfCode
+print("Hey I am a good boy \nand this" 
+"viewer is also a good boy/girl")
+
+print("Hello World") #printing Hello World
+
+print("Hey I am a \"Good Boy\"") #Using escape sequence to print double quotes
+
+print("Hey",6,7,sep="~",end="$$") #Using sep and end parameters in print function
