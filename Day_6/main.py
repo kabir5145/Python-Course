b8c2dcad1581222c50d0bf59ee6625c8839b1120
kabir5145 <kabir10112006@gmail.com>
2025-10-18 00:58:57 +0530

    Variables and Data Types in python course,Day 6

---
+++ Day_6/main.py
@@ -0,0 +1,26 @@
+# Day 6 - Variables and Data Types
+
+a = 1
+b = True
+c = "kabir"
+d = None 
+a1 = 9
+g = complex(2,3)
+print(g)
+print(a)
+print(b)
+print(a-a1)
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+
+# List
+list1 = [8,2.3,[4,5],['apple','banana']]
+print(list1)
+
+# Tuple
+tuple1 = (("parrot","sparrow"),('cat','dog'))
+print(tuple1)
+
+# Mapped data : dict
+dict1 = {"name" : "kabir","age" : 21,"is_student" : True}
+print(dict1)
