1fc04a6c4a5258677345f20bc235f29dc637d134
kabir5145 <kabir10112006@gmail.com>
2025-10-20 17:29:36 +0530

    Strings in python, Day -11

---
+++ Day-11/main.py
@@ -0,0 +1,21 @@
+# Strings in python
+name = "kabir"
+friend = 'lovish'
+apple = "He said, \"I want to eat an apple\""
+
+# multi-line string
+multi_line_string = '''Dear Kabir,
+This is a multi-line string example.
+Have a great day!
+Regards,
+Admin'''
+
+print(name[0])
+print(name[1])
+# print(apple)
+print(multi_line_string)
+
+# Looping through strings
+print("Using a for loop :")
+for character in friend:
+    print(character)
