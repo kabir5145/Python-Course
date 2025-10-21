7f31f3ab0e0b081664d2964c4fa7b43388b7146d
kabir5145 <kabir10112006@gmail.com>
2025-10-21 13:25:12 +0530

    Day -12, Strings Slicing and Operations on Strings

---
+++ Day -12/main.py
@@ -0,0 +1,15 @@
+# Strings Slicing and Operations on Strings in python
+
+my_name = "Kabir,Harry"
+print(my_name[0:5])  # Slicing from index 0 to 4
+
+# Length of a string
+print("Length of a string is :",len(my_name))  # Output: 11
+fruit = "Mango"
+print(fruit[0:4]) # Slicing from index 0 to 3
+print(fruit[-4:0]) # Slicing using negative indexing from -4 to 0
+print(fruit[::2]) # Slicing with step of 2
+
+# Quick quiz:
+# nm  = "Harry"
+# print(nm[-4:-2])
