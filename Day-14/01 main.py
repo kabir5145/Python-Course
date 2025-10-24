14d68db115d1e02e49f386afdf2b310be657421d
kabir5145 <kabir10112006@gmail.com>
2025-10-24 21:39:11 +0530

    Day-14 , If Else Conditional Statements

---
+++ Day-14/01 main.py
@@ -0,0 +1,16 @@
+# if-else statements
+
+a = int(input("Enter your age : "))
+print("Your age is : ",a)
+
+# Conditional operators
+    #  > , < , >= , <= , == , !=
+# print(a>18)
+# print(a<=18)
+# print(a==18)
+# print(a!=18)
+if(a>=18):
+    print("You can drive")
+else:
+    print("You canot drive")
+
---
+++ Day-14/04 nested.py
@@ -0,0 +1,14 @@
+num = int(input("Enter The Num : "))
+
+if(num<0):
+    print("Number is Negative : ", num)
+elif(num>0):
+    if(num<=10):
+        print("Number is between 1-10")
+    elif(num >10 and num <= 20):
+        print("Number is between 11-20")
+    elif(num>20):
+        print("Number is greater than 20")
+else:
+    print("Number is zero")
+    
