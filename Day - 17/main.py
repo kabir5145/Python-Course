bf0b919f2f1c22491035a0a6304b15aca53ebbef
kabir5145 <kabir10112006@gmail.com>
2026-01-22 23:50:14 +0530

    Day - 17 For loops in python

---
+++ Day - 17/main.py
@@ -0,0 +1,21 @@
+# name = "Kabir"
+
+# for i in name:
+#     print(i)
+#     if i == "b":
+#         print("Found the letter b!")
+        
+
+# colors = ["red", "green", "blue"]
+
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+
+# n = range(1, 5)
+# for i in n:
+#     print(i)
+
+for k in range(1,11,2):
+    print(k)
