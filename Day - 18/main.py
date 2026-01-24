4f0490c599325b215ea47a7317b186e67f956bd9
kabir5145 <kabir10112006@gmail.com>
2026-01-24 22:56:58 +0530

    While loop in python

---
+++ Day - 18/main.py
@@ -0,0 +1,13 @@
+# i =  int(input("Enter a number: "))
+# while(i<=3):
+#     i = int(input("Enter a number: "))
+#     print(i)
+
+# print("Loop ended")
+
+count = 5
+while(count>0):
+    print("Count is:", count)
+    count -= 1
+else:
+    print("I am inside else")
