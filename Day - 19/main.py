755d839900278f1b99000f8dec9dd9aa6d8ff42e
kabir5145 <kabir10112006@gmail.com>
2026-01-27 23:38:35 +0530

    Day - 19 break and continue in Python

---
+++ Day - 19/main.py
@@ -0,0 +1,15 @@
+# for i in range(12):
+#     if(i == 10):
+#         print("Skip the iteration when i is 10")
+#         continue
+#         # break
+#     print("5 *", i, "=", 5 * i)
+
+# print("Loop ended.")    
+
+i = 0
+while True:
+    print(i)
+    i += 1
+    if(i % 100 == 0):
+        break
