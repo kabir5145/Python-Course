bf9bbfee97d4b53ad1a21e44b80031927abe0308
kabir5145 <kabir10112006@gmail.com>
2026-01-28 23:40:36 +0530

    Day - 20 Functions in Python

---
+++ Day - 20/main.py
@@ -0,0 +1,14 @@
+def calculateGmean(a,b):
+    return (a*b)/(a+b)
+
+print(calculateGmean(4,5))
+
+def isGreater(a,b):
+    if a > b:
+        return True
+    else:
+        return False
+    
+def isLesser(a,b):
+    pass   
+print(isGreater(4,5))
