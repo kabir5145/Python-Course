55d3a0e7e8be5826a0087a382763e204733f0a2b
Kabir5145 <kabir10112006@gmail.com>
2026-02-02 22:37:17 +0530

    Day - 22 Introduction to lists in python

---
+++ Day - 22/main.py
@@ -0,0 +1,30 @@
+marks = [1,3,4,5,6,"Kabir",7]
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+# # marks.append(7)
+# print(marks)
+
+# # Negative Indexing
+# print(marks[-3])
+# print(marks[-1])
+
+# if 7 in marks:
+#     print("Yes")
+# else:
+#     print("No")
+
+# Same thing applies for strings as well
+# name = "Kabir"
+# if "a" in name:
+#     print("Yes")
+
+# print(marks)
+# print(marks[1:5])  # Slicing
+# print(marks[::2])  # Skipping
+
+# Reversing a list
+# print(marks[::-1])
+
+lst = [ i*i for i in range(1,11) if i%2==0 ]
+print(lst)
