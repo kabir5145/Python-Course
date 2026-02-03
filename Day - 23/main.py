bdb5cb265b3989225d26e4c8522d08365a26def8
Kabir5145 <kabir10112006@gmail.com>
2026-02-03 23:02:01 +0530

    List Method in python

---
+++ Day - 23/main.py
@@ -0,0 +1,33 @@
+l = [32,77,34,47,59]
+
+# Adding elements
+# l.append(6)
+# print(l)  
+
+# sorting elements
+# l.sort()
+# print(l)  
+ 
+# Reversing elements
+# l.reverse()
+# print(l)  
+
+# Finding index of an element
+# print(l.index(32))  
+
+# Counting occurrences of an element
+# print(l.count(34))
+
+# Copying the list
+# l2 = l.copy()
+# print(l2)
+
+# Inserting an element at a specific position
+# l.insert(2, 100)
+# print(l)
+
+m = [900 , 800, 700]
+# Extending the list by another list
+# l.extend(m)
+k = l + m
+print(k)
