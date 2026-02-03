5cd64bea2e0cb954cd59de7bb338d477e018cb0f
Kabir5145 <kabir10112006@gmail.com>
2026-02-03 23:20:18 +0530

    Operations on tuples in python

---
+++ Day - 25/main.py
@@ -0,0 +1,13 @@
+# Operations on tuples in Python
+
+# Creating a tuple
+my_tuple = (1, 2, 3, 4, 5, 3, 6, 7, 8, 9)
+print("Original Tuple:", my_tuple)
+
+# Accessing elements
+first_element = my_tuple[0]
+print("First Element:", first_element)
+
+# Index of an element
+index_of_3 = my_tuple.index(3,4,5)
+print("Index of 3:", index_of_3)
