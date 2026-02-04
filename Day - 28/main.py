94ef0b20ac35541fa72fee53e7402d04a772ae30
Kabir5145 <kabir10112006@gmail.com>
2026-02-04 22:51:36 +0530

    Day - 28 f-strings in python

---
+++ Day - 28/main.py
@@ -0,0 +1,15 @@
+# f-stringgs
+# letter = "Hey my name is {0}, I am {1} years old"
+name = "Alice"
+age = 30
+
+# print(letter.format(name,age))
+
+print(f"Hey my name is {name}, I am {age} years old")
+
+# txt = "For only {price:.2f} dollars!"
+# price = 49.99999
+# print(txt.format(price=price))
+
+txt = f"For only {49.99999:.2f} dollars!"
+print(txt)
